c05e827480032d5c3bacb4073ce849447489e350
chathu_08 <chathu.rash97@gmail.com>
2022-03-30 05:12:17 +0530

    Implemented Factory Method Pattern

--- Factory-Method/src/com/company/Main.java
+++ Factory-Method/src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+	    GetPlanFactory planFactory = new GetPlanFactory();
+
+        System.out.println("Enter name of the plan: ");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String planName = bufferedReader.readLine();
+
+        System.out.println("Enter number of units: ");
+        int units = Integer.parseInt(bufferedReader.readLine());
+
+        Plan plan = planFactory.getPlan(planName);
+
+        System.out.println("Amount for " + planName + " for " + units + " units is: ");
+        plan.getRate();
+        plan.calculateBill(units);
     }
 }
