d923d818ea05922cfaf80249e59d6625eab5471f
chathu_08 <chathu.rash97@gmail.com>
2022-04-03 06:16:02 +0530

    Implemented the prototype design pattern

--- 1. Creational Design Patterns/4. Prototype-Design-Pattern/src/com/company/Main.java
+++ 1. Creational Design Patterns/4. Prototype-Design-Pattern/src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(System.in)));
+
+        System.out.println("Employee id: ");
+        int employeeId = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println("Employee name: ");
+        String employeeName = bufferedReader.readLine();
+
+        System.out.println("Employee designation: ");
+        String employeeDesignation = bufferedReader.readLine();
+
+        System.out.println("Employee address: ");
+        String employeeAddress = bufferedReader.readLine();
+
+        System.out.println("Employee salary: ");
+        double employeeSalary = Double.parseDouble(bufferedReader.readLine());
+
+        EmployeeRecord employee_1 = new EmployeeRecord(employeeId, employeeName, employeeDesignation, employeeSalary, employeeAddress);
+        employee_1.showRecord();
+
+        EmployeeRecord employee_2 = (EmployeeRecord) employee_1.getClone();
+        employee_2.showRecord();
     }
 }
